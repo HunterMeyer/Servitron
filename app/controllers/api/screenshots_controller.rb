57c449f2f2b50c7c130866e04987eb3105e19c75
HunterMeyer <huntermeyer@live.com>
2017-01-17 21:39:47 -0500

    Allow instant screenshot creation

--- app/controllers/api/screenshots_controller.rb
+++ app/controllers/api/screenshots_controller.rb
@@ -2,8 +2,14 @@ module Api
   class ScreenshotsController < ApiController
     def create
       @screenshot = Screenshot.create(screenshot_params)
-      if @screenshot.save && @screenshot.enqueue
-        render json: { id: @screenshot.id, message: 'Talk to you soon' }
+      if @screenshot.save
+        if params[:instant] == 'true'
+          @screenshot.process!
+          render json: { file_url: @screenshot.file_url }
+        else
+          @screenshot.enqueue
+          render json: { id: @screenshot.id, message: 'Talk to you soon' }
+        end
       end
     end
 
