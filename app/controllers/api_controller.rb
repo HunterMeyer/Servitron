56156cd3b0bfeb775edbe435ed5081b1d8c158e8
HunterMeyer <huntermeyer@live.com>
2017-01-25 12:09:56 -0500

    Restrict API to users with API Key

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,24 @@
 class ApiController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_action :validate_api_key
+
+  private
+
+  def validate_api_key
+    return missing_api_key unless params.key?(:key)
+    return access_denied unless current_api_user.try(:api_enabled?)
+  end
+
+  def current_api_user
+    return @current_api_user if defined? @current_api_user
+    @current_api_user = User.find_by_api_key(params[:key])
+  end
+
+  def missing_api_key
+    access_denied('Missing API Key')
+  end
+
+  def access_denied(message = 'Access Denied')
+    render json: { error: message }, status: :unauthorized
+  end
 end
